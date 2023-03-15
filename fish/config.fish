6fb7b25f662d02b1a507f69842043b5739dc72da
Nicolas Missika <nmissika@synthesio.com>
2023-03-15 11:25:24 +0100

    remove bullshit from fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,13 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+  # Commands to run in interactive sessions can go here
+  abbr -a "got" "git"
+  abbr -a "gut" "git"
+  abbr -a "lv" "lvim"
+  abbr -a "n" "nvim"
+  abbr -a "nuke" "rm -rf"
+  abbr -a "l" "ls -l"
+  abbr -a "ll" "ls -la"
 end
+
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
