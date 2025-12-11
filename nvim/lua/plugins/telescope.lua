6ea1565416632fc7ca72a268d78f5d3be3f81a97
Nicolas Missika <nicolas.missika@aive.com>
2025-12-11 14:54:01 +0100

    update nvim

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,8 +5,7 @@ return {
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function ()
-
+    config = function()
       -- [[ Configure Telescope ]]
       -- See `:help telescope` and `:help telescope.setup()`
       require('telescope').setup {
@@ -24,22 +23,27 @@ return {
       pcall(require('telescope').load_extension, 'fzf')
 
       local function fileDiagnostics()
-        return require("telescope.builtin").diagnostics({bufnr = 0})
+        return require("telescope.builtin").diagnostics({ bufnr = 0 })
       end
 
       -- See `:help telescope.builtin`
       vim.keymap.set('n', "<leader>'", require('telescope.builtin').resume, { desc = "['] Open last picker" })
+      vim.keymap.set('n', "<leader>g", require('telescope.builtin').git_status, { desc = "[g] Changed files" })
       vim.keymap.set('n', '<leader>b', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
       vim.keymap.set('n', '<leader>j', require('telescope.builtin').jumplist, { desc = '[ ] Find existing buffers' })
       vim.keymap.set('n', '<leader>f', require('telescope.builtin').find_files, { desc = 'open [f]iles picker' })
       vim.keymap.set('n', '<leader>h', require('telescope.builtin').help_tags, { desc = 'open [H]elp picker' })
       vim.keymap.set('n', '<leader>w', require('telescope.builtin').grep_string, { desc = 'Search current [W]ord' })
-      vim.keymap.set('n', '<leader>/', require('telescope.builtin').live_grep, { desc = 'Global search [/] in workspace' })
+      vim.keymap.set('n', '<leader>/', require('telescope.builtin').live_grep,
+        { desc = 'Global search [/] in workspace' })
       vim.keymap.set('n', '<leader>?', require('telescope.builtin').keymaps, { desc = 'Open keymaps' })
-      vim.keymap.set('n', '<leader>d', require("telescope.builtin").diagnostics,{ desc =  'Open [d]iagnostics picker for file' })
-      vim.keymap.set('n', '<leader>D', fileDiagnostics,{ desc =  'Open [D]iagnostics picker for all opened files' })
-      vim.keymap.set('n', '<leader>s', require('telescope.builtin').lsp_document_symbols, { desc =  'Open [s]ymbols picker' })
-      vim.keymap.set('n', '<leader>S', require('telescope.builtin').lsp_dynamic_workspace_symbols, { desc =  'Open workspace [S]ymbols picker' })
+      vim.keymap.set('n', '<leader>d', require("telescope.builtin").diagnostics,
+        { desc = 'Open [d]iagnostics picker for file' })
+      vim.keymap.set('n', '<leader>D', fileDiagnostics, { desc = 'Open [D]iagnostics picker for all opened files' })
+      vim.keymap.set('n', '<leader>s', require('telescope.builtin').lsp_document_symbols,
+        { desc = 'Open [s]ymbols picker' })
+      vim.keymap.set('n', '<leader>S', require('telescope.builtin').lsp_dynamic_workspace_symbols,
+        { desc = 'Open workspace [S]ymbols picker' })
     end
 
   },
